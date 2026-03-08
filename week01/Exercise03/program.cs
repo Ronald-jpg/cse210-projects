4a9aa1f6d05b99323a84ddcf364249660bcd236b
Ronald-jpg <kakaronald95@gmail.com>
2026-03-08 16:00:24 +0300

    Add files via upload

---
+++ week01/Exercise03/program.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Generate a random number between 1 and 100
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    }
+}
